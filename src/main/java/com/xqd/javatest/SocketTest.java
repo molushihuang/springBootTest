07da914bcd4caea1f48c1fdedd32b3f11bb44315
xqd <xieqiudongv5@163.com>
2019-08-07 17:35:47 +0800

    socket服务端测试代码

---
+++ src/main/java/com/xqd/javatest/SocketTest.java
@@ -0,0 +1,56 @@
+package com.xqd.javatest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketTest {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket();
+            serverSocket.bind(new InetSocketAddress("192.168.3.93", 9998));
+            System.out.println("服务器启动");
+            while (true){
+                Socket socket = serverSocket.accept();
+                new ReaderThread(socket).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    static class ReaderThread extends Thread {
+
+        BufferedReader mBufferedReader;
+
+        public ReaderThread(Socket socket) {
+            try {
+                mBufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            super.run();
+
+            String content = null;
+            while (true) {
+                try {
+                    if (((content = mBufferedReader.readLine()) != null)) {
+                        System.out.println("接收到了客户端：" + content);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+}
