7c1774584a374bb021080744b759bc1d9e810659
xqd <xieqiudongv5@163.com>
2019-10-24 17:51:37 +0800

    leetcode代码

--- src/main/java/com/xqd/javatest/Test.java
+++ src/main/java/com/xqd/javatest/Test.java
@@ -34,27 +34,8 @@ public class Test {
 //        System.out.println("hashTableR = " + hashTableR / 10);
 
         Test test = new Test();
-//        test.treeMapSortTest();
-        int[] nums = {3, 2, 7, 11, 15};
-        int[] t = test.twoSum(nums, 9);
-
-        System.out.println(t[0]);
-        System.out.println(t[1]);
+        test.treeMapSortTest();
     }
-
-    public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    result[0] = i;
-                    result[1] = j;
-                }
-            }
-        }
-        return result;
-    }
-
 
     public void test(int size) {
         int index;
--- src/main/java/com/xqd/javatest/leetcode/LeetCodeTest.java
+++ src/main/java/com/xqd/javatest/leetcode/LeetCodeTest.java
@@ -10,12 +10,14 @@ class LeetCodeTest {
 
         LeetCodeTest test = new LeetCodeTest();
 //        test.treeMapSortTest();
-        int[] nums = {3, 2, 7, 11, 15};
-        int[] t = test.twoSum(nums, 9);
+//        int[] nums = {3, 2, 7, 11, 15};
+//        int[] t = test.twoSum(nums, 9);
+//
+//        System.out.println(t[0]);
+//        System.out.println(t[1]);
 
-        System.out.println(t[0]);
-        System.out.println(t[1]);
-
+        int num = test.strStr("aaaaa", "bba");
+        System.out.println(num);
     }
 
     //两书相加
@@ -32,22 +34,82 @@ class LeetCodeTest {
         return result;
     }
 
-
-    public String longestCommonPrefix(String[] strs) {
-        if(strs.length==0){
+    //最长公共前缀（我的）
+    public String longestCommonPrefix2(String[] strs) {
+        if (strs.length == 0) {
             return "";
         }
-        String str=strs[0];
-        String cur="";
-        for(int i=1;i<str.length()+1;i++){
-            String select=str.substring(0,i);
-            for(String s:strs){
-                if(s.contains(select)){
-                    cur=select;
+        String str = strs[0];
+        String cur = "";
+        for (int i = 1; i < str.length() + 1; i++) {
+            String select = str.substring(0, i);
+            for (String s : strs) {
+                if (s.contains(select)) {
+                    cur = select;
                 }
             }
         }
         return cur;
+    }
+
+    //最长公共前缀(官方)
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        return prefix;
+    }
+
+    //删除数组中的重复项
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+
+    }
+
+    //移除给定的数组元素
+    public int removeElement(int[] nums, int val) {
+        if (nums.length == 0)
+            return 0;
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+
+    }
+
+    //实现indexOf方法
+    public int strStr(String haystack, String needle) {
+
+        if (needle == null || needle.length() == 0) {
+            return 0;
+        }
+        int len = needle.length();
+        int index = -1;
+
+        for (int i = 0; i < haystack.length(); i++) {
+            if (i + len <= haystack.length() && needle.equals(haystack.substring(i, i + len))) {
+                index = i;
+                break;
+            }
+
+        }
+        return index;
 
     }
 }
