b31e6c8b8700c73d201d77992b22d2e93bf0329b
xqd <xieqiudongv5@163.com>
2019-11-14 17:30:46 +0800

    杨辉三角算法

--- src/main/java/com/xqd/javatest/leetcode/LeetCodeTest.java
+++ src/main/java/com/xqd/javatest/leetcode/LeetCodeTest.java
@@ -1,6 +1,8 @@
 package com.xqd.javatest.leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by 谢邱东 on 2019/10/23 15:07.
@@ -175,7 +177,29 @@ class LeetCodeTest {
         Arrays.sort(nums1);
     }
 
+    //杨辉三角
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> yang = new ArrayList<>();
+        if (numRows == 0) {
+            return yang;
+        }
+        yang.add(new ArrayList<Integer>());
+        yang.get(0).add(1);
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> rows = new ArrayList<>();
+            List<Integer> preRows = yang.get(i - 1);
 
+            rows.add(1);
+            for (int j = 1; j < i; j++) {
+                rows.add(preRows.get(j - 1) + preRows.get(j));
+            }
+            rows.add(1);
+
+            yang.add(rows);
+
+        }
+        return yang;
+    }
 }
 
 
