620a1d8ac05c2b6e7ddb8fde97b9ef542812447d
xqd <xieqiudongv5@163.com>
2019-11-18 17:40:20 +0800

    买股票的最佳时机

--- src/main/java/com/xqd/javatest/leetcode/LeetCodeTest.java
+++ src/main/java/com/xqd/javatest/leetcode/LeetCodeTest.java
@@ -200,6 +200,20 @@ class LeetCodeTest {
         }
         return yang;
     }
+
+    //买卖股票的最佳时机
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
 }
 
 
